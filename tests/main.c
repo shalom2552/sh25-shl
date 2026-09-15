95c1ee5e481068c5e3a7ba9aa20ebcf1af86efa3
shalom2552 <shalom2552@gmail.com>
2026-09-15 13:59:44 +0300

    refactor(tests): use sh25_test lib for tests

--- tests/main.c
+++ tests/main.c
@@ -1,30 +1,7 @@
-#include <stdio.h>
-#include <unistd.h>
+#define SH25_UTEST_IMPLEMENTATION
+#include "sh25_utest.h"
 
-int test_stack(void);
-
-static int total = 0;
-#define TEST(fn) test(fn, #fn)
-
-static void test(int (*test_suite)(void), const char* name)
-{
-    printf("Running %s...\n", name);
-
-    int count = test_suite();
-    total += count;
-
-    printf("%s: \t%d tests passed\n\n", name, count);
-}
-
-int main(void)
-{
-    printf("sh25 unit tests\n");
-    printf("=================================================\n");
-
-    TEST(test_stack);
-
-    printf("=================================================\n");
-    printf("\nTotal %d tests passed!\n", total);
-    return 0;
+TEST_MAIN {
+    RUN_SUITE(test_stack);
 }
 
--- tests/test_stack.c
+++ tests/test_stack.c
@@ -1,64 +1,52 @@
 #define SH25_STACK_IMPLEMENTATION
 #include "sh25_stack.h"
 
-#include <stddef.h>
-#include <assert.h>
-#include <stdio.h>
+#include "sh25_utest.h"
 
-#define RUN(fn) (test(fn, #fn), ++count)
-
-static void test(void (*unit_test)(void), const char* name)
-{
-    printf("  Running %s...", name);
-    fflush(stdout);
-    unit_test();
-    printf(" [OK]\n");
-}
-
-static void test_stack_init(void)
+TEST(test_stack_init)
 {
     stack_t stack;
     stack_init(stack, sizeof(int));
-    assert(stack.size == 0);
+    ASSERT_EQ(stack.size, 0);
 
     stack_destroy(stack);
 }
 
-static void test_stack_push(void)
+TEST(test_stack_push)
 {
     stack_t stack;
     stack_init(stack, sizeof(int));
 
     int a = 1;
     stack_push(stack, a);
-    assert(stack.size == 1);
+    ASSERT_EQ(stack.size, 1);
 
     int pop;
     stack_pop(stack, pop);
-    assert(pop == a);
-    assert(stack.size == 0);
+    ASSERT_EQ(pop, a);
+    ASSERT_EQ(stack.size, 0);
 
     stack_destroy(stack);
 }
 
-static void test_stack_push_same_element(void)
+TEST(test_stack_push_same_element)
 {
     stack_t stack;
     stack_init(stack, sizeof(int));
 
     int a = 1;
     stack_push(stack, a);
-    assert(stack.size == 1);
+    ASSERT_EQ(stack.size, 1);
 
     int pop;
     stack_pop(stack, pop);
-    assert(pop == a);
-    assert(stack.size == 0);
+    ASSERT_EQ(pop, a);
+    ASSERT_EQ(stack.size, 0);
 
     stack_destroy(stack);
 }
 
-static void test_stack_push_twice(void)
+TEST(test_stack_push_twice)
 {
     stack_t stack;
     stack_init(stack, sizeof(int));
@@ -66,23 +54,23 @@ static void test_stack_push_twice(void)
     int a = 1;
     int b = 2;
     stack_push(stack, a);
-    assert(stack.size == 1);
+    ASSERT_EQ(stack.size, 1);
     stack_push(stack, b);
-    assert(stack.size == 2);
+    ASSERT_EQ(stack.size, 2);
 
     int pop;
     stack_pop(stack, pop);
-    assert(pop == b);
-    assert(stack.size == 1);
+    ASSERT_EQ(pop, b);
+    ASSERT_EQ(stack.size, 1);
 
     stack_pop(stack, pop);
-    assert(pop == a);
-    assert(stack.size == 0);
+    ASSERT_EQ(pop, a);
+    ASSERT_EQ(stack.size, 0);
 
     stack_destroy(stack);
 }
 
-static void test_stack_peek(void)
+TEST(test_stack_peek)
 {
     stack_t stack;
     stack_init(stack, sizeof(int));
@@ -93,20 +81,20 @@ static void test_stack_peek(void)
     stack_push(stack, a);
 
     stack_peek(stack, peek);
-    assert(peek == a);
-    assert(stack.size == 1);
+    ASSERT_EQ(peek, a);
+    ASSERT_EQ(stack.size, 1);
 
     stack_push(stack, b);
-    assert(stack.size == 2);
+    ASSERT_EQ(stack.size, 2);
 
     stack_peek(stack, peek);
-    assert(peek == b);
-    assert(stack.size == 2);
+    ASSERT_EQ(peek, b);
+    ASSERT_EQ(stack.size, 2);
 
     stack_destroy(stack);
 }
 
-static void test_stack_clear(void)
+TEST(test_stack_clear)
 {
     stack_t stack;
     stack_init(stack, sizeof(int));
@@ -116,57 +104,55 @@ static void test_stack_clear(void)
     stack_push(stack, a);
 
     stack_clear(stack);
-    assert(stack_size(stack) == 0);
-    assert(stack_empty(stack));
-    assert(stack.size == 0);
+    ASSERT_EQ(stack_size(stack), 0);
+    ASSERT(stack_empty(stack));
+    ASSERT_EQ(stack.size, 0);
 
     stack_peek(stack, value);
-    assert(value == 5);
-    assert(stack.size == 0);
+    ASSERT_EQ(value, 5);
+    ASSERT_EQ(stack.size, 0);
 
     stack_destroy(stack);
 }
 
-static void test_stack_drop(void)
+TEST(test_stack_drop)
 {
     stack_t stack;
     stack_init(stack, sizeof(int));
 
     int a = 1;
     stack_push(stack, a);
-    assert(stack.size == 1);
+    ASSERT_EQ(stack.size, 1);
     stack_drop(stack);
-    assert(stack.size == 0);
-    assert(stack_empty(stack));
+    ASSERT_EQ(stack.size, 0);
+    ASSERT(stack_empty(stack));
     stack_destroy(stack);
 }
 
-static void test_stack_size(void)
+TEST(test_stack_size)
 {
     stack_t stack;
     stack_init(stack, sizeof(int));
 
     int a = 1;
     for (int i = 0; i < STACK_INITIAL_CAPACITY * 2 + 2; ++i) {
-        assert(stack.size == (size_t)i);
-        assert(stack_size(stack) == i);
+        ASSERT_EQ(stack.size, (size_t)i);
+        ASSERT_EQ(stack_size(stack), i);
         stack_push(stack, a);
     }
     stack_destroy(stack);
 }
 
 // === MAIN ===================================================================
-int test_stack(void)
+TEST_SUITE(test_stack)
 {
-    int count = 0;
-    RUN(test_stack_init);
-    RUN(test_stack_push);
-    RUN(test_stack_push_same_element);
-    RUN(test_stack_push_twice);
-    RUN(test_stack_peek);
-    RUN(test_stack_clear);
-    RUN(test_stack_drop);
-    RUN(test_stack_size);
-    return count;
+    RUN_TEST(test_stack_init);
+    RUN_TEST(test_stack_push);
+    RUN_TEST(test_stack_push_same_element);
+    RUN_TEST(test_stack_push_twice);
+    RUN_TEST(test_stack_peek);
+    RUN_TEST(test_stack_clear);
+    RUN_TEST(test_stack_drop);
+    RUN_TEST(test_stack_size);
 }
 
